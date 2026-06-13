d26c7509df0437d4ddf4d5df2a1246ad8db74db9
Hector Astudillo del Valle <hector.astudillo@yahoo.es>
2026-06-13 13:25:47 +0200

    posts, tags and astro api

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://stupendous-chebakia-7ce03b.netlify.app/"
+});
