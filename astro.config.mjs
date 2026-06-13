ce171c88263321c5c0db8a6cfe74c03ba9959a7e
Hector Astudillo del Valle <hector.astudillo@yahoo.es>
2026-06-13 19:00:32 +0200

    preact island and theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://stupendous-chebakia-7ce03b.netlify.app/"
+  site: "https://stupendous-chebakia-7ce03b.netlify.app/",
+  integrations: [preact()]
 });
